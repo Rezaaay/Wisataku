9858bb296bbf4709a77e6c4928246ce8c93f893d
Rezaaay <95263965+Rezaaay@users.noreply.github.com>
2023-03-29 14:15:53 +0700

    Add files via upload

---
+++ Porto1fix/Porto1fix/Porto1fixApp.swift
@@ -0,0 +1,17 @@
+//
+//  Porto1fixApp.swift
+//  Porto1fix
+//
+//  Created by Naufal Fachreza on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Porto1fixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
